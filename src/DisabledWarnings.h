24a54d57e94abd5e3bc819fa847f71b285e8ce19
Toni Mäkinen <masfonos@gmail.com>
2021-09-30 18:51:23 +0300

    Initial commit

---
+++ src/DisabledWarnings.h
@@ -0,0 +1,13 @@
+
+
+#pragma once
+#pragma warning(disable : 5039)   // pointer or reference to potentially throwing function passed
+                                  // to 'extern "C"' function under -EHc. Undefined behavior may occur if this
+                                  // function throws an exception.
+
+#ifndef _DEBUG
+#  pragma warning(disable : 4710)   // <function> function not inlined
+#  pragma warning(disable : 4711)   // <function> selected for automatic inline expansion
+#endif
+
+
