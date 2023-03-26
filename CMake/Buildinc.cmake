86bd49c3f0600027d0bb66ee1ef5887ab06623cb
Toni Mäkinen <masfonos@gmail.com>
2023-03-26 15:12:55 +0300

    Add buildinc to cmake

--- CMake/Buildinc.cmake
+++ CMake/Buildinc.cmake
@@ -1,8 +1,10 @@
-add_custom_command(TARGET buildinc
-    POST_BUILD
-    COMMENT Run BuildInc and copy to tools
-    COMMAND buildinc.exe buildnumber.h BuildInc -q
-    #COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/bin/buildinc.exe E:/tools/global_commands
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src
-    USES_TERMINAL
-)
+function(run_buildinc target)
+
+    add_custom_command(TARGET ${target}
+        PRE_BUILD
+        COMMENT Run BuildInc and copy to tools
+        COMMAND buildinc.exe buildnumber.h BuildInc -q
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src
+        USES_TERMINAL
+    )
+endfunction()
--- src/main.cpp
+++ src/main.cpp
@@ -133,10 +133,8 @@ std::string uuid_string() noexcept
 {
     static std::random_device                      rd;
     static std::uniform_int_distribution<uint64_t> dist(0);
-
     uint64_t ab = (dist(rd) & 0xFFFFFFFFFFFF0FFFULL) | 0x0000000000004000ULL;
     uint64_t cd = (dist(rd) & 0x3FFFFFFFFFFFFFFFULL) | 0x8000000000000000ULL;
-
 
     return std::format("{:08X}-{:04X}-{:04X}-{:04X}-{:012X}",
                        (ab >> 32) & 0xFFFF'FFFF,
