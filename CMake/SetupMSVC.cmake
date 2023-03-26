bb94d8f3aab52b23c20eaee6672688f0d8ed2980
Toni Mäkinen <masfonos@gmail.com>
2023-03-26 19:45:58 +0300

    CMake: simplify cmake script

--- CMake/SetupMSVC.cmake
+++ CMake/SetupMSVC.cmake
@@ -145,4 +145,14 @@ function(setup_piku_executable_msvc target outputname)
 
     endif() # MSVC
 
+    find_program(BUILDINC_TOOL "buildinc" NO_CACHE)
+
+    add_custom_command(TARGET ${target}
+                       PRE_BUILD
+                       COMMAND ${BUILDINC_TOOL} ${CMAKE_CURRENT_SOURCE_DIR}/src/buildnumber.h BuildInc -q 
+                       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src
+                       COMMENT "Increase build number"
+
+    )
+
 endfunction()
