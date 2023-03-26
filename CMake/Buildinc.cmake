9f09f73887c64759f1c51d81bbbace0d4f115f5a
Toni Mäkinen <masfonos@gmail.com>
2023-03-26 17:01:51 +0300

    More cmake build versioning

--- CMake/Buildinc.cmake
+++ CMake/Buildinc.cmake
@@ -1,8 +1,11 @@
 function(run_buildinc target)
 
+        find_program(BUILDINC_TOOL "buildinc" NO_CACHE)
+
         add_custom_command(TARGET ${target}
                            PRE_BUILD
-                           COMMAND buildinc ${CMAKE_CURRENT_SOURCE_DIR}/src/buildnumber.h BuildInc -q 
+                           COMMAND ${BUILDINC_TOOL} ${CMAKE_CURRENT_SOURCE_DIR}/src/buildnumber.h BuildInc -q 
+                           WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src
                            COMMENT "Increase build number"
 
         )
