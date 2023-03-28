41e892e6b11132deadc987258167fb5332086099
Toni Mäkinen <masfonos@gmail.com>
2023-03-28 22:07:08 +0300

    Update clang-format and the action

--- src/DisabledWarnings.h
+++ src/DisabledWarnings.h
@@ -1,16 +1,16 @@
 
 
 #pragma once
-#pragma warning(disable : 5039)   // pointer or reference to potentially throwing function passed
-                                  // to 'extern "C"' function under -EHc. Undefined behavior may occur if this
-                                  // function throws an exception.
+#pragma warning(disable : 5039) // pointer or reference to potentially throwing function passed
+								// to 'extern "C"' function under -EHc. Undefined behavior may occur if this
+								// function throws an exception.
 
 #ifndef _DEBUG
-#    pragma warning(disable : 4710)   // <function> function not inlined
-#    pragma warning(disable : 4711)   // <function> selected for automatic inline expansion
+	#pragma warning(disable : 4710) // <function> function not inlined
+	#pragma warning(disable : 4711) // <function> selected for automatic inline expansion
 #endif
 
-#pragma warning(disable : 5045)   // Compiler will insert Spectre mitigation for memory
-                                  // load if /Qspectre switch specified
+#pragma warning(disable : 5045) // Compiler will insert Spectre mitigation for memory
+								// load if /Qspectre switch specified
 
-#pragma warning(disable: )
+#pragma warning(disable:)
--- src/buildnumber.h
+++ src/buildnumber.h
@@ -4,42 +4,41 @@
 #pragma once
 
 #ifndef BUILDINCVERSION_BUILDNUMBER_HEADER_H
-#define BUILDINCVERSION_BUILDNUMBER_HEADER_H
+	#define BUILDINCVERSION_BUILDNUMBER_HEADER_H
 
-#include <cstdint>
+	#include <cstdint>
 
 namespace BuildIncVersion
 {
-		 // You can modify major and minor
-		constexpr uint32_t major = 0;
-		constexpr uint32_t minor = 2;
+	// You can modify major and minor
+	constexpr uint32_t major = 0;
+	constexpr uint32_t minor = 2;
 
-		 // Do not modify these
-		constexpr uint32_t build = 267;
-		
-		constexpr uint32_t version = major * 10000 + minor * 1000 + build;
-		constexpr uint64_t random_seed = 0xb27c591baee45f52;
-		
-		constexpr char version_string[] = "v0.2.267";
-		constexpr char build_time_string[] = "2023-03-28 17:40:33";
-		constexpr char phrase[] = "labored-impulse-array";
-		constexpr char calver[] = "2023.13.267";
-		constexpr char uuid[] = "0F3245FF-ECF1-46C1-BA25-D5F065E8F794";
+	// Do not modify these
+	constexpr uint32_t build = 267;
 
-		// Copy paste to import to your project
-		/*
-			constexpr auto major = BuildIncVersion::major;
-			constexpr auto minor = BuildIncVersion::minor;
-			constexpr auto build = BuildIncVersion::build;
-			constexpr auto version = BuildIncVersion::version;
-			constexpr auto random_seed = BuildIncVersion::random_seed;
-			
-			constexpr auto version_string = BuildIncVersion::version_string;
-			constexpr auto build_time_string = BuildIncVersion::build_time_string;
-			constexpr auto phrase = BuildIncVersion::phrase;
-			constexpr auto calver = BuildIncVersion::calver;
-			constexpr auto uuid = BuildIncVersion::uuid;
-		*/
+	constexpr uint32_t version     = major * 10000 + minor * 1000 + build;
+	constexpr uint64_t random_seed = 0xb27c'591b'aee4'5f52;
+
+	constexpr char version_string[]    = "v0.2.267";
+	constexpr char build_time_string[] = "2023-03-28 17:40:33";
+	constexpr char phrase[]            = "labored-impulse-array";
+	constexpr char calver[]            = "2023.13.267";
+	constexpr char uuid[]              = "0F3245FF-ECF1-46C1-BA25-D5F065E8F794";
+
+	// Copy paste to import to your project
+	/*
+		constexpr auto major = BuildIncVersion::major;
+		constexpr auto minor = BuildIncVersion::minor;
+		constexpr auto build = BuildIncVersion::build;
+		constexpr auto version = BuildIncVersion::version;
+		constexpr auto random_seed = BuildIncVersion::random_seed;
+
+		constexpr auto version_string = BuildIncVersion::version_string;
+		constexpr auto build_time_string = BuildIncVersion::build_time_string;
+		constexpr auto phrase = BuildIncVersion::phrase;
+		constexpr auto calver = BuildIncVersion::calver;
+		constexpr auto uuid = BuildIncVersion::uuid;
+	*/
 }
 #endif // BUILDINCVERSION_BUILDNUMBER_HEADER_H
-
